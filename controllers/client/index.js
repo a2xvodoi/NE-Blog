b42a2ce9040e1cd4cbd21f66da4d2304611cb8bd
Việt Nguyễn <vietnguyen2x@gmail.com>
2021-05-31 09:58:20 +0700

    add layout client

--- controllers/client/index.js
+++ controllers/client/index.js
@@ -1,6 +1,6 @@
 module.exports ={
     index : (req, res, next) =>{
-        res.render('index', { title: 'Blog' });
+        res.render('client/index', { title: 'Blog' });
     },
     user: (req, res, next) =>{
         res.send('Page of user');
