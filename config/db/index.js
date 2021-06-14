01f8d83defaa5ac536029b174d2c46f20957a22b
a2xvodoi <vietnguyen2x@gmail.com>
2021-06-14 13:46:07 +0700

    add plugin tinycme

--- config/db/index.js
+++ config/db/index.js
@@ -3,7 +3,7 @@ const connection = mysql.createConnection({
   host     : 'localhost',
   port     : '3306',
   user     : 'blog',
-  password : '123456',
+  password : 'Viet161100*',
   database: 'blog',
 });
  
